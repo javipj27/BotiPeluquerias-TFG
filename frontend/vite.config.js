d2814f297d727d68b8eec801250a5d64d4e44685
javierperezjimenez <javier.perezjimenez@movired.com>
2025-06-09 20:07:39 +0200

    dns y certificados funcionaaaaando letss go!

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
-// https://vite.dev/config/
 export default defineConfig({
+  server:{
+        allowedHosts: ['botipeluquerias.work.gd']
+  },
   plugins: [
     react(),
     tailwindcss(),
